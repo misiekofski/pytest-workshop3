901387a908bd86275a94b5ba6ffeb214a7c42614
Michal Dobrzycki <dobrzycki@gmail.com>
2023-03-23 22:49:47 +0100

    now this code needs to actually compile

--- test.cpp
+++ test.cpp
@@ -1,1 +1,11 @@
-is_temp(space)?assert(size>=5):assert(size>=7);
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    space = false;
+    size = 4;
+    is_temp(space)?assert(size>=5):assert(size>=7);
+
+    return 0;
+}
