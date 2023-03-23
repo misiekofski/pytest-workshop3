714efade72ceceb28d622ee8fcfdad640d88741d
Michal Dobrzycki <dobrzycki@gmail.com>
2023-03-23 22:53:23 +0100

    I have no idea what i'm doing

--- test.cpp
+++ test.cpp
@@ -2,9 +2,13 @@
 #include <fstream>
 using namespace std;
 
+bool is_temp(bool bool_value) {
+    return !bool_value;
+}
+
 int main() {
-    space = false;
-    size = 4;
+    bool space = false;
+    int size = 4;
     is_temp(space)?assert(size>=5):assert(size>=7);
 
     return 0;
